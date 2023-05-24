134eef30de812d7dce84d5037cb1fa0d5a600269
Souparno Paul <souparnopaul@gmail.com>
2023-05-24 22:57:04 +0530

    [+] Performance Metric in terms of nanoseconds elapsed

--- src/Main.java
+++ src/Main.java
@@ -19,10 +19,20 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
 
+        // Start time
+        long start = System.nanoTime();
+
         // Adding Game Objects to Camera scene
         camera.gameObjects.add(new Vector(500, 500, Color.CYAN));
         camera.gameObjects.add(new Vector(540, 360, Color.CYAN));
         camera.gameObjects.add(new LineSegment(100, 200, 500, 600, Color.GREEN));
+        camera.gameObjects.add(new LineSegment(700, 500, 900, 300, Color.RED));
+
+        // End time
+        long end = System.nanoTime();
+        // Elapsed time
+        long elapsed = end - start;
+        System.out.println("Time elapsed(in ns) = " + elapsed);
 
         frame.getContentPane().add(camera);
         frame.setVisible(true);
